b6a6e75ccc76caf0fd644aa6f1008c8ee0112c44
Zach <zach.gollwitzer@gmail.com>
2021-04-10 17:32:37 -0400

    Remove solution files from master branch

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleNav() {
-  const outerGrid = document.querySelector(".outer-grid");
-  outerGrid.classList.toggle("outer-grid-expanded");
-
-  const nav = document.querySelector("nav");
-  nav.classList.toggle("hide-nav");
-}
